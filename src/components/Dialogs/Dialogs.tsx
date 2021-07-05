b5f06a183832d63f650598099e0a7924d86c10c4
Dmity-kl <i6169696@gmail.com>
2021-07-05 22:36:58 +0300

    Unit_24_final

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -26,16 +26,32 @@ const Message = (props: MessageType) => {
 }
 
 export function Dialogs() {
+    let dialogsData = [
+        {id: 1, name: 'Dima'},
+        {id: 2, name: 'Oleg'},
+        {id: 3, name: 'Jenya'},
+        {id: 4, name: 'Misha'},
+        {id: 5, name: 'Andrey'},
+    ]
+
+    let messageData = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you'},
+        {id: 3, message: 'Ok'},
+
+    ]
+
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-               <DialogItem name={'Dima'} id={1}/>
+               <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
                <DialogItem name={'Oleg'} id={2}/>
                <DialogItem name={'Jenya'} id={3}/>
                <DialogItem name={'Misha'} id={4}/>
             </div>
             <div className={s.messages}>
-                <Message message={'Hello'}/>
+                <Message message={messageData[0].message}/>
                 <Message message={'How are you'}/>
                 <Message message={'Ok'}/>
             </div>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,6 +4,12 @@ import s from './MyPosts.module.css'
 
 
 export function MyPosts() {
+    let postsData = [
+        {id: 1, message: 'Hello guys', likesCount: 15},
+        {id: 2, message: 'My name is Dima\'', likesCount: 15},
+    ]
+
+
     return (
         <div className={s.myPosts}>
             <div>
@@ -19,8 +25,9 @@ export function MyPosts() {
                         <button>Add post</button>
                     </div>
 
-                    <Post message={'Hello guys'} likeCount={15}/>
-                    <Post message={'My name is Dima'} likeCount={20}/>
+                    <Post message={postsData[0].message} likeCount={postsData[0].likesCount}/>
+                    <Post message={postsData[1].message} likeCount={postsData[1].likesCount}/>
+
                 </div>
             </div>
         </div>
